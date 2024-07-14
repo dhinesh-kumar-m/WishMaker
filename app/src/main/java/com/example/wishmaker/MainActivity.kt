590d0866f4c4e015d1d4fa30ce920d7e029d9a63
Dhineshdk <dinesh.k131997@gmail.com>
2024-07-15 00:59:27 +0530

    Add image composable

--- app/src/main/java/com/example/wishmaker/MainActivity.kt
+++ app/src/main/java/com/example/wishmaker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wishmaker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     WishMakerTheme {
-        GreetingText(message = "Good Night", "From Dhinesh",modifier = Modifier.padding(8.dp)
+        GreetingImage(message = "Good Night", "From Dhinesh"
         )
     }
 }
@@ -67,3 +69,13 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         )
     }
 }
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
